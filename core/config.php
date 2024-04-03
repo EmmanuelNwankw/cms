f738b50b3363e68375723d366a05fd76f37628f6
EmmanuelNwankw <monreal2020@gmail.com>
2024-04-03 12:49:24 +0100

    v6

---
+++ core/config.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace core;
+
+class config {
+    private static $config = [
+        'version'            =>'0.0.1',
+        'root_dir'           =>'/cms',  // This will likely be / on a live server
+        'default_controller' => 'blog', // The default home controller
+        'default_layout'     => 'default',  // Default layout that is used
+        'default_site_title' => 'Freeskills',  // Default site title
+        'db_host'            => '127.0.0.1',  // DB host please use IP address no domain
+        'db_name'            => 'cms',  // DB name
+        'db_user'            => 'root',  //Db user
+        'db_password'        => '',  // DB password
+        'login_cookie_name'  => 'hakdjri23418la', // Login cookie name
+
+    ];
+}
